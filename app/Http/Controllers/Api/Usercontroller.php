17c5306aa874e32feb640a9e59248ca2a510686c
YinChantha <yin.chantha19@kit.edu.kh>
2023-10-04 09:54:08 +0700

    Authentication with sanctum

---
+++ app/Http/Controllers/Api/Usercontroller.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Validator;
+use Auth;
+use App\Models\User;
+
+class Usercontroller extends Controller
+{
+    public function register(Request $request)
+    {
+        // validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'c_password' => 'required|same:password'
+        ]);
+
+        if ($validator->fails()) {
+            $response = [
+                'success' => false,
+                'message' => $validator->errors()
+            ];
+            return response()->json($response, 400);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($input['password']);
+        $user = User::create($input);
+
+        $success['token'] = $user->createToken('Myapp')->plainTextToken;
+        $success['name'] = $user->name;
+
+        $response = [
+            'success' => true,
+            'data' => $success,
+            'message' => 'User registered successfully'
+        ];
+        return response()->json($response, 200);
+    }
+
+    public function login(Request $request)
+    {
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('Myapp')->plainTextToken;
+            $success['name'] = $user->name;
+
+            $response = [
+                'success' => true,
+                'data' => $success,
+                'message' => 'User logged in successfully'
+            ];
+            return response()->json($response, 200);
+        } else {
+            $response = [
+                'success' => false,
+                'message' => 'Unauthorized',
+            ];
+            return response()->json($response);
+        }
+    }
+}
